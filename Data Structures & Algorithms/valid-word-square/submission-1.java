9a5b42077b52b9295c68cf11744db9bd7a79cc68
Valeri Putnin <putnin.v@gmail.com>
2026-07-01 21:35:45 +0300

    Add: valid-word-square - submission-1

---
+++ Data Structures & Algorithms/valid-word-square/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean validWordSquare(List<String> words) {
+        // 14.40
+        for(int i = 0; i < words.size(); i++) {
+            String word = words.get(i);
+
+            for(int j = 0; j < word.length(); j++) {
+                if(j >= words.size()) {
+                    return false;
+                }
+
+                if(i >= words.get(j).length()) {
+                    return false;
+                }
+
+
+
+                if(word.charAt(j) != words.get(j).charAt(i)) {
+                    return false;
+                }
+            }
+
+        }
+
+        return true;
+    }
+}
