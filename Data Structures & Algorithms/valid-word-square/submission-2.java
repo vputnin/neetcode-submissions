68131e84e92e3451d5062ec31d8f20aae3341a4f
Valeri Putnin <putnin.v@gmail.com>
2026-07-01 21:37:15 +0300

    Add: valid-word-square - submission-2

---
+++ Data Structures & Algorithms/valid-word-square/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean validWordSquare(List<String> words) {
+        // 14.40 
+        //Firstly I've skiped some requirements, then it was difficult to understand how to reflect.
+        //solved it in several hours 
+        for(int i = 0; i < words.size(); i++) {
+            String word = words.get(i);
+
+            for(int j = 0; j < word.length(); j++) {
+                if(j >= words.size()) {
+                    return false;
+                }
+
+                if(i >= words.get(j).length()) {
+                    return false;
+                }
+
+
+
+                if(word.charAt(j) != words.get(j).charAt(i)) {
+                    return false;
+                }
+            }
+
+        }
+
+        return true;
+    }
+}
