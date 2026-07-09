5354b2395d377c425c9174e688c0d1bf6d3be629
Valeri Putnin <putnin.v@gmail.com>
2026-07-09 22:13:38 +0300

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int climbStairs(int n) {
+        //spent about 5 minutes, but I rewrote solution with memo bottom up
+        if(n == 1) {
+            return 1;
+        }
+        int[] memo = new int[n + 1];
+
+        int stage1 = 1;
+        int stage2 = 2;
+        memo[1] = 1;
+        memo[2] = 2;
+
+        for(int i = 3; i <= n; i++) {
+            memo[i] = memo[i-1] + memo[i - 2];
+        }
+
+        return memo[n];
+    }
+}
