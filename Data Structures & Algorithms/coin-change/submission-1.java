ceefaf8a32048239c526a85e625b9f6fc9f06d20
Valeri Putnin <putnin.v@gmail.com>
2026-07-12 17:41:29 +0300

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        // Spent about 10 minutes. Before that spent about 2 days two visualize this task and recurent function. 
+        int dp[] = new int[amount + 1];
+
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+
+        for(int i = 1; i <= amount; i++) {
+            for(int j = 0; j < coins.length; j++) {
+                if(coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+    }
+    
+}
