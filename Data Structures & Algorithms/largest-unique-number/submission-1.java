41d1a8c43219f64a9cef1feac5bfc639b697b610
Valeri Putnin <putnin.v@gmail.com>
2026-07-13 13:32:59 +0300

    Add: largest-unique-number - submission-1

---
+++ Data Structures & Algorithms/largest-unique-number/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int largestUniqueNumber(int[] nums) {
+        // 03 30 
+        // 27 minutes 
+        // At first, I wanted to solve everything in a single loop, but then I realized that I needed two passes to mark duplicate values in first loop and then to go through unique 
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int max = -1;
+
+        for(int i = 0; i < nums.length; i++) {
+            if(map.get(nums[i]) == null) {
+                map.put(nums[i], 1);
+            } else {
+                map.put(nums[i], 2);
+            }
+        }
+
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if(entry.getValue() == 1) {
+                max = Math.max(max, entry.getKey());
+            }
+        }
+
+        return max;
+
+    }
+}
