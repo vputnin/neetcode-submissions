4de955a1158b7980ff4135a0a8dda6aecee5e201
Valeri Putnin <putnin.v@gmail.com>
2026-07-23 08:06:46 +0300

    Add: move-zeroes - submission-0

---
+++ Data Structures & Algorithms/move-zeroes/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+        int right = 0;
+
+        while(right < nums.length) {
+            if(nums[right] != 0) {
+
+                int tmp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = tmp;
+                left++;
+            }
+            right++;
+        }
+    }
+}
