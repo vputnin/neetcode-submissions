64693154f35f4bde36894260ccb535510a93201f
Valeri Putnin <putnin.v@gmail.com>
2026-07-24 15:57:05 +0300

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.java
@@ -0,0 +1,46 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length; i ++) {
+            if(i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+
+            int left = i+1;
+            int right = nums.length - 1;
+
+            while(left < right) {
+                
+
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0) {
+                    List<Integer> res1 = new ArrayList<>();
+                    res1.add(nums[i]);
+                    res1.add(nums[left]);
+                    res1.add(nums[right]);
+                    res.add(res1);
+                    left++;
+                    right--;
+
+                    while(left < right && nums[left] == nums[left - 1]) {
+                        left+=1;
+                    }
+                    while(left < right && nums[right] == nums[right + 1]) {
+                        right-=1;
+                    }
+                } else if(sum > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+
+                
+            }
+
+        }
+
+        return res;
+    }
+}
