0dcf28629b66e57c3fcff98b33382c6b2a179b8e
Valeri Putnin <putnin.v@gmail.com>
2026-07-24 17:02:47 +0300

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int left = 0;
+        int right = 0;
+
+        while(right < nums.length) {
+            if(nums[right] == val) {
+                right++;
+                continue;
+            } else {
+                nums[left] = nums[right];
+                left++;
+            }
+            right++;
+        }
+        return left;
+    }
+}
