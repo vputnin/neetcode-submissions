da5879636d52455f14e1dfb6b0664726ca4ddb46
Valeri Putnin <putnin.v@gmail.com>
2026-07-24 19:25:47 +0300

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int left = 0;
+        int medium = 0;
+        int right = nums.length - 1;
+
+        while(medium <= right) {
+            if(nums[medium] == 0) {
+                swap(nums, left, medium);
+                left++;
+                medium++;
+            } else if(nums[medium] == 1) {
+                medium++;
+            } else {
+                swap(nums, right, medium);
+                right--;
+            }
+        }
+    }
+
+    public void swap(int[] nums, int left, int right) {
+        int tmp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = tmp;
+    }
+}
